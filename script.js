516ffac47369cb274a63153222c75e44946f9fe0
Logesh-134 <rlogesh134@gmail.com>
2026-04-21 19:12:05 +0530

    Implemented create post

--- script.js
+++ script.js
@@ -1,22 +1,85 @@
+//API URL
 const apiFile = "https://jsonplaceholder.typicode.com/posts";
 
+const app = document.getElementById("app");
+
+// Fetch data from API
 fetch(apiFile)
   .then(resp => resp.json())
-  .then(posts => {
-    const app = document.getElementById("app");
+  .then(gets => {
 
-    posts.forEach(post => {
+    // loop post rendering
+    gets.forEach(get => {
       const card = document.createElement("div");
       card.className = "card";
 
       const title = document.createElement("h3");
-      title.textContent = post.title;
+      title.textContent = get.title;
 
       const body = document.createElement("p");
-      body.textContent = post.body;
+      body.textContent = get.body;
 
       card.append(title, body);
       app.appendChild(card);
     });
   })
+  // Error Handler
   .catch(err => console.log("Error:", err));
+
+
+  
+// Create data and send to API
+const form = document.getElementById("create");
+const titleInput = document.getElementById("title");
+const bodyInput = document.getElementById("body");
+
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    alert("Form Submitted");
+
+    // create newPost object
+    const newPost = {
+      title: titleInput.value,
+      body: bodyInput.value
+    };
+   
+    fetch(apiFile, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newPost)
+    })
+
+    .then(resp => resp.json())
+    .then(post => {
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const title = document.createElement("h3");
+    title.textContent = post.title;
+
+    const body = document.createElement("p");
+    body.textContent = post.body;
+
+    card.append(title, body);
+
+    // Add new post at top
+    app.prepend(card);
+
+    // Clear form
+    titleInput.value = "";
+    bodyInput.value = "";
+  })
+   
+  .catch(err => console.log("Error:",err));
+
+});
+
+
+
+
+
+
+
