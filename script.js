2c7102edc8b309a7b2a1fb089fda69cb36dcc629
Logesh-134 <rlogesh134@gmail.com>
2026-04-22 17:44:52 +0530

    Added update functionality

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const app = document.getElementById("app");
 
 // Fetch data from API
 fetch(apiFile)
-  .then(resp => resp.json())
-  .then(gets => {
-
+  .then((resp) => resp.json())
+  .then((gets) => {
     // loop post rendering
-    gets.forEach(get => {
+    gets.forEach((get) => {
       const card = document.createElement("div");
       card.className = "card";
 
@@ -19,67 +18,152 @@ fetch(apiFile)
       const body = document.createElement("p");
       body.textContent = get.body;
 
-      card.append(title, body);
+      const editBtn = document.createElement("button");
+      editBtn.textContent = "Edit";
+      editBtn.className = "editBtn";
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "Delete";
+      deleteBtn.className = "deleteBtn";
+
+      //create wrapper
+      const btnContainer = document.createElement("div");
+      btnContainer.className = "btn-group";
+
+      // Edit button
+      editBtn.addEventListener("click", function () {
+        modal.style.display = "block";
+
+        editTitle.value = get.title;
+        editBody.value = get.body;
+
+        currentPostId = get.id;
+        currentCard = card;
+      });
+
+      btnContainer.append(editBtn, deleteBtn);
+      card.append(title, body, btnContainer);
       app.appendChild(card);
     });
   })
   // Error Handler
-  .catch(err => console.log("Error:", err));
+  .catch((err) => console.log("Error:", err));
 
-
-  
 // Create data and send to API
 const form = document.getElementById("create");
 const titleInput = document.getElementById("title");
 const bodyInput = document.getElementById("body");
 
-form.addEventListener("submit",function(e){
-    e.preventDefault();
-    alert("Form Submitted");
+  btn.addEventListener("click", function (e) {
+  e.preventDefault(); //prevent default reload on webpage
+  alert("Form Submitted");
 
-    // create newPost object
-    const newPost = {
-      title: titleInput.value,
-      body: bodyInput.value
-    };
-   
-    fetch(apiFile, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newPost)
+  //Check empty inputz
+  const title = titleInput.value.trim();
+  const body = bodyInput.value.trim();
+
+  if (title === "" || body === "") {
+    return; // stop here (not API call, not create card)
+  }
+
+  // create newPost object
+  const newPost = {
+    title: title,
+    body: body,
+  };
+
+  // post new data
+  fetch(apiFile, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPost),
+  })
+    .then((resp) => resp.json())
+    .then((post) => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      const title = document.createElement("h3");
+      title.textContent = post.title;
+
+      const body = document.createElement("p");
+      body.textContent = post.body;
+
+      const editBtn = document.createElement("button");
+      editBtn.textContent = "Edit";
+      editBtn.className = "editBtn";
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "Delete";
+      deleteBtn.className = "deleteBtn";
+
+      //create wrapper
+      const btnContainer = document.createElement("div");
+      btnContainer.className = "btn-group";
+
+      btnContainer.append(editBtn, deleteBtn);
+      card.append(title, body, btnContainer);
+
+      // Add new post at top
+      app.prepend(card);
+
+      // Clear form
+      titleInput.value = "";
+      bodyInput.value = "";
     })
 
-    .then(resp => resp.json())
-    .then(post => {
-
-    const card = document.createElement("div");
-    card.className = "card";
-
-    const title = document.createElement("h3");
-    title.textContent = post.title;
-
-    const body = document.createElement("p");
-    body.textContent = post.body;
-
-    card.append(title, body);
-
-    // Add new post at top
-    app.prepend(card);
-
-    // Clear form
-    titleInput.value = "";
-    bodyInput.value = "";
-  })
-   
-  .catch(err => console.log("Error:",err));
-
+    // Error Handler
+    .catch((err) => console.log("Error:", err));
 });
 
+// Update the Existing data from API
+const modal = document.getElementById("modal");
+const editTitle = document.getElementById("editTitle");
+const editBody = document.getElementById("editBody");
+const saveBtn = document.getElementById("saveBtn");
+const closeBtn = document.getElementById("closeBtn");
 
+let currentPostId = null;
+let currentCard = null;
 
+// Update button
+saveBtn.addEventListener("click", function () {
+  const updatedTitle = editTitle.value.trim();
+  const updatedBody = editBody.value.trim();
 
+  if (updatedTitle === "" || updatedBody === "") {
+    return;
+  }
 
+  // update post
+  const updatePost = {
+    title: updatedTitle,
+    body: updatedBody,
+  };
 
+  fetch(`${apiFile}/${currentPostId}`, {
+    method: "PATCH", // or PUT
 
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(updatePost),
+  })
+    .then((resp) => resp.json())
+    .then((patch) => {
+      currentCard.querySelector("h3").textContent = patch.title;
+      currentCard.querySelector("p").textContent = patch.body;
+
+      modal.style.display = "none";
+    })
+
+    .catch((err) => console.log("Error:", err));
+});
+
+//Close Modal
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
